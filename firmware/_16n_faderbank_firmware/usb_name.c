b837f67bd6106d006c613280e5b3a2fd2f2afcf1
Tom Armitage <tom@infovore.org>
2018-02-20 22:43:33 +0000

    Rename MIDI device to '16n'.
    Note that for a Mac to detect this change you'll need to disconnect the 16n,
    go to "Audio/MIDI preferences", find the "Teensy MIDI" device and click 'remove'.
    Then, next time you connect 16n, it'll appear in your MIDI devices as '16n'.

---
+++ firmware/_16n_faderbank_firmware/usb_name.c
@@ -0,0 +1,10 @@
+#include <usb_names.h>
+
+#define PRODUCT_NAME    {'1','6','n'}
+#define PRODUCT_NAME_LEN 3
+
+struct usb_string_descriptor_struct usb_string_product_name = {
+        2 + PRODUCT_NAME_LEN * 2,
+        3,
+        PRODUCT_NAME
+};
